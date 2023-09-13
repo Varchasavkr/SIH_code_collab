d72295415332882bc782b82614b4b212ed0a6fb4
varchasavkr <varchasav25@gmail.com>
2023-09-13 12:27:32 +0530

    userview implemented without navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Landing from "./components/Landing";
+import Userview from "./components/Userview";
 import React, { useEffect, useState } from "react";
+import "./App.css"
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="overflow-x-hidden font-Inter ">
       <Routes>
         <Route path="/" Component={Landing} exact />
+        <Route path="/User" Component={Userview} exact />
       </Routes>
     </div>
   );
